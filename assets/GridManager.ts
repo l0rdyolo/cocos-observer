108654c6864193220594aad3a27c244bce5645da
Omer <62906173+l0rdyolo@users.noreply.github.com>
2024-12-10 17:09:04 +0300

    colorfyyy grid

--- assets/GridManager.ts
+++ assets/GridManager.ts
@@ -1,4 +1,6 @@
-import { _decorator, Component, Node, Prefab, instantiate, Vec3, UITransform } from 'cc';
+
+import { _decorator, Component, Node, Prefab, instantiate, Vec3, UITransform, Sprite, Color } from 'cc';
+import { Cell } from './Cell';
 const { ccclass, property } = _decorator;
 
 @ccclass('GridManager')
@@ -10,10 +12,17 @@ export class GridManager extends Component {
     public parentNode: Node = null;
 
     @property
-    public gridSize: number = 5;
+    public levelConfig: number[][] = [
+        [0, 1, 2, 3, 0],
+        [1, 2, 3, 0, 1],
+        [2, 3, 0, 1, 2],
+        [3, 0, 1, 2, 3],
+        [0, 1, 2, 3, 0]
+    ];
 
     @property
-    public cellSpacing: number = 10; 
+    public cellSpacing: number = 10;
+
 
     start() {
         this.createGrid();
@@ -25,16 +34,18 @@ export class GridManager extends Component {
             return;
         }
 
-        const cellSize = this.cellPrefab.data.getComponent(UITransform).contentSize; 
-        const startX = -((this.gridSize - 1) * (cellSize.width + this.cellSpacing)) / 2;
-        const startY = -((this.gridSize - 1) * (cellSize.height + this.cellSpacing)) / 2;
+        const cellSize = this.cellPrefab.data.getComponent(UITransform).contentSize;
+        const startX = -((this.levelConfig[0].length - 1) * (cellSize.width + this.cellSpacing)) / 2;
+        const startY = -((this.levelConfig.length - 1) * (cellSize.height + this.cellSpacing)) / 2;
 
-        for (let row = 0; row < this.gridSize; row++) {
-            for (let col = 0; col < this.gridSize; col++) {
+        for (let row = 0; row < this.levelConfig.length; row++) {
+            for (let col = 0; col < this.levelConfig[row].length; col++) {
+                const type = this.levelConfig[row][col];
                 const cellNode = instantiate(this.cellPrefab);
+                const cellComp = cellNode.getComponent(Cell)
+                cellComp.init(type);
                 const posX = startX + col * (cellSize.width + this.cellSpacing);
                 const posY = startY + row * (cellSize.height + this.cellSpacing);
-
                 cellNode.setPosition(new Vec3(posX, posY, 0));
                 this.parentNode.addChild(cellNode);
             }
